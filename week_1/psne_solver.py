9989dd61bfee9319960435df1a6cd2153965725d
Akshat-Srivastava-IITK12 <sakshat25@iitk.ac.in>
2026-05-20 19:25:39 +0530

    Add PSNE solver function with user input
    Implement function to solve for pure strategy Nash equilibria in a game. Add user input for payoff matrices.

--- week_1/psne_solver.py
+++ week_1/psne_solver.py
@@ -1,1 +1,39 @@
+def solve_psne(p1_matrix, p2_matrix):
+  psne = []
+  row = len(p1_matrix)
+  col = len(p1_matrix[0])
+  p1_best = []
+  p2_best = []
+  for j in range(col):
+    max_payoff = -10000
+    for i in range(row):
+      max_payoff = max(max_payoff, p1_matrix[i][j])
+    for i in range(row):
+      if p1_matrix[i][j] == max_payoff:
+        p1_best.append((i, j))
+  for i in range(row):
+    max_payoff = -10000
+    for j in range(col):
+      max_payoff = max(max_payoff, p2_matrix[i][j])
+    for j in range(col):
+      if p2_matrix[i][j] == max_payoff:
+        p2_best.append((i, j))
+  for x in p1_best:
+    if x in p2_best:
+      psne.append(x)
+  return psne
 
+N = int(input("Enter Number of Rows: "))
+M = int(input("Enter Number of Cols. : "))
+p1_matrix = []
+p2_matrix = []
+row = []
+print("Enter payoff for P1 : ")
+for i in range(N):
+  row = list(map(int, input().split()))
+  p1_matrix.append(row)
+print("Enter payoff for P2 : ")
+for i in range(N):
+  row = list(map(int, input().split()))
+  p2_matrix.append(row)
+print(solve_psne(p1_matrix, p2_matrix))
