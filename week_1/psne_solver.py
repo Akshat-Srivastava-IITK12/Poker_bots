e759f00a25649ee564b5a9d4adb5cd454670d46d
Akshat-Srivastava-IITK12 <sakshat25@iitk.ac.in>
2026-05-21 18:08:56 +0530

    Refactor PSNE solver for clarity and optimization

--- week_1/psne_solver.py
+++ week_1/psne_solver.py
@@ -1,27 +1,27 @@
 def solve_psne(p1_matrix, p2_matrix):
   psne = []
+  p1_optimum = []
+  p2_optimum = []
   row = len(p1_matrix)
   col = len(p1_matrix[0])
-  p1_best = []
-  p2_best = []
+  max_val = -10000
   for j in range(col):
-    max_payoff = -10000
     for i in range(row):
-      max_payoff = max(max_payoff, p1_matrix[i][j])
+        max_val = max(max_val, p1_matrix[i][j])
     for i in range(row):
-      if p1_matrix[i][j] == max_payoff:
-        p1_best.append((i, j))
+        if p1_matrix[i][j] == max_val:
+            p1_optimum.append((i, j))
   for i in range(row):
-    max_payoff = -10000
     for j in range(col):
-      max_payoff = max(max_payoff, p2_matrix[i][j])
+      max_val = max(max_val, p2_matrix[i][j])
     for j in range(col):
-      if p2_matrix[i][j] == max_payoff:
-        p2_best.append((i, j))
-  for x in p1_best:
-    if x in p2_best:
+      if p2_matrix[i][j] == max_val:
+        p2_optimum.append((i, j))
+  for x in p1_optimum:
+    if x in p2_optimum:
       psne.append(x)
   return psne
+
 
 N = int(input("Enter Number of Rows: "))
 M = int(input("Enter Number of Cols. : "))
@@ -30,10 +30,10 @@ p2_matrix = []
 row = []
 print("Enter payoff for P1 : ")
 for i in range(N):
-  row = list(map(int, input().split()))
-  p1_matrix.append(row)
+    row = list(map(int, input().split()))
+    p1_matrix.append(row)
 print("Enter payoff for P2 : ")
 for i in range(N):
-  row = list(map(int, input().split()))
-  p2_matrix.append(row)
+    row = list(map(int, input().split()))
+    p2_matrix.append(row)
 print(solve_psne(p1_matrix, p2_matrix))
